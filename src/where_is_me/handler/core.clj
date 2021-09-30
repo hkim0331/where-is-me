d672a17fc4b2887e2970a4bd49051b0f9ec90dfe
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-09-30 22:13:33 +0900

    release 0.2.4, session

--- src/where_is_me/handler/core.clj
+++ src/where_is_me/handler/core.clj
@@ -6,7 +6,7 @@
             [where-is-me.boundary.locations :as locs]
             [where-is-me.view :as view]))
 
-(def ^:private version "0.2.3")
+(def ^:private version "0.2.4")
 
 (defmethod ig/init-key :where-is-me.handler.core/version [_ _]
   (fn [_]
